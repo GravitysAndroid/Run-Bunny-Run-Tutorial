84b388de23b294063433fd15f3bd301de9fe8b64
Hayden <55818671+GravitysAndroid@users.noreply.github.com>
2020-07-30 17:40:22 +1200

    Added animation to run and jump for the bunny
    Added inq  the animation for running and jumping along with the scripting side of it. The Script means that when space is pressed it will change to the jump sprite and switch back once the bunny is falling.

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class BunnyController : MonoBehaviour
 {
     private Rigidbody2D myRigedBody;
+    private Animator myAnim;
     public float bunnyJumpForce = 500f;
     // Start is called before the first frame update
     void Start()
     {
-        myRigedBody = GetComponent<Rigidbody2D>(); 
+        myRigedBody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,5 +21,7 @@ public class BunnyController : MonoBehaviour
         {
             myRigedBody.AddForce(transform.up * bunnyJumpForce);
         }
+
+        myAnim.SetFloat("vVelocity", myRigedBody.velocity.y);
     }
 }
