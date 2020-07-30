f5b7ac3b3d6f474aacb836c93e9aa9db91e6a52c
Hayden <55818671+GravitysAndroid@users.noreply.github.com>
2020-07-30 17:17:24 +1200

    Chnaged the location of my games files

---
+++ Assets/Scripts/BunnyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyController : MonoBehaviour
+{
+    private Rigidbody2D myRigedBody;
+    public float bunnyJumpForce = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigedBody = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetButtonUp("Jump"))
+        {
+            myRigedBody.AddForce(transform.up * bunnyJumpForce);
+        }
+    }
+}
