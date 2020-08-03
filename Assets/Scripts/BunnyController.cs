f9800f2e05a56b21e3055afd1ee8e2271023e0d4
Hayden <55818671+GravitysAndroid@users.noreply.github.com>
2020-08-03 18:48:39 +1200

    Up to adding sound effects & music. #10

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -2,35 +2,88 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BunnyController : MonoBehaviour
 {
     private Rigidbody2D myRigedBody;
     private Animator myAnim;
     public float bunnyJumpForce = 500f;
+    private float bunnyHurtTime = -1;
+    private Collider2D myCollider;
+    public Text scoreText;
+    private float startTime;
+    private int jumpsLeft = 2;
+
     // Start is called before the first frame update
     void Start()
     {
         myRigedBody = GetComponent<Rigidbody2D>();
         myAnim = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
+
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (Input.GetButtonUp("Jump"))
+        if (bunnyHurtTime == -1)
         {
-            myRigedBody.AddForce(transform.up * bunnyJumpForce);
-        }
+            if (Input.GetButtonUp("Jump") && jumpsLeft > 0)
+            {
+                if (myRigedBody.velocity.y < 0)
+                {
+                    myRigedBody.velocity = Vector2.zero;
+                }
 
-        myAnim.SetFloat("vVelocity", myRigedBody.velocity.y);
+                if (jumpsLeft == 1)
+                {
+                    myRigedBody.AddForce(transform.up * bunnyJumpForce * 0.75f);
+                }
+                else
+                {
+                    myRigedBody.AddForce(transform.up * bunnyJumpForce);
+                }
+
+                jumpsLeft--;
+            }
+
+            myAnim.SetFloat("vVelocity", myRigedBody.velocity.y);
+            scoreText.text = (Time.time - startTime).ToString("0.0");
+        }
+        else
+        {
+            if (Time.time > bunnyHurtTime + 2)
+            {
+                SceneManager.LoadScene("GameScene");
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
-            Application.LoadLevel(Application.loadedLevel);
+            foreach (PrefabSpawner spawner in FindObjectsOfType<PrefabSpawner>())
+            {
+                spawner.enabled = false;
+            }
+
+            foreach (MoveLeft moveLefter in FindObjectsOfType<MoveLeft>())
+            {
+                moveLefter.enabled = false;
+            }
+
+            bunnyHurtTime = Time.time;
+            myAnim.SetBool("bunnyHurt", true);
+            myRigedBody.velocity = Vector2.zero;
+            myRigedBody.AddForce(transform.up * bunnyJumpForce);
+            myCollider.enabled = false;
+        }
+        else if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            jumpsLeft = 2;
         }
     }
 }
